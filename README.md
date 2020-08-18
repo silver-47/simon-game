96a31f46236632caad9a11f86a1fd3f4b5ef775f
silver-47 <arnab.ganguly0047@gmail.com>
2020-08-18 15:53:49 +0530

    Second Version

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ Simon Game by Silver © 2020
 A Simon Game with Default Rules...  
 
 # Description  
-This is the First Version of the Website.  
-It is made with EJS as Front-End & Express.js + Node.js as Back-End.  
-It has no Database Connection. So, A Server-Restart will Result in Data Loss.  
+This is the Second Version of the Website.  
+It is made with EJS as Front-End & Express.js + Node.js as Back-End & MongoDB Atlas is used as Database.  
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -11,15 +13,20 @@ app.use((req, res, next) => {
   if (req.secure) next();
   else res.redirect('https://' + req.headers.host + req.url);
 });
+mongoose.connect(process.env.DB_URL,
+  {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}
+);
 
-let leaders = [
-  {name: 'None', score: 0},
-  {name: 'None', score: 0},
-  {name: 'None', score: 0}
-];
+const User = mongoose.model("User", {
+  name: String,
+  highScore: {type: Number, default: 0},
+  scores: [Number]
+});
 
 app.get('/', (req, res) => {
-  res.render('getName', {leaders});
+  User.find().select('name highScore')
+  .sort({highScore: -1}).limit(3)
+  .exec((err, leaders) => res.render('getName', {leaders}));
 });
 
 app.post('/', (req, res) => {
@@ -34,8 +41,17 @@ app.get('/playas/:name', (req, res) => {
 app.post('/playas/:name', (req, res) => {
   const {name} = req.params;
   const score = parseInt(req.body.score);
-  leaders = leaders.concat({name, score}).sort((a,b)=>b.score-a.score).slice(0,3);
-  res.status(201).end();
+  User.findOneAndUpdate({name}, {
+      name,
+      $push: {scores: score},
+    },
+    {upsert: true, new: true, setDefaultsOnInsert: true},
+    (err, result) => {
+      User.updateOne({name: result.name}, {
+        $set: {highScore: (score > result.highScore)? score : result.highScore}
+      }, (er) => {if (!er) res.status(201).end();});
+      if (!err) res.status(201).end();
+    });
 });
 
 
